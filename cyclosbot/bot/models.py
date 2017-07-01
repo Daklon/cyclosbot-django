669f57bd5cb0829fd6b354f8936b13aa0ac1c217
Javier Herrera <muchorrollodejavi@hotmail.com>
2017-07-01 12:24:00 -0400

    bugs fixed

--- cyclosbot/bot/models.py
+++ cyclosbot/bot/models.py
@@ -6,7 +6,11 @@ from django.contrib.auth.models import User
 class TelegramUser(models.Model):
     registered = models.BooleanField(default=False)
     chat_id = models.AutoField(primary_key=True)
-    django_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    # django_user = models.ForeignKey(User, on_delete=models.PROTECT)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20) # i don't like this way to store the password but i has to do it
     conversation_status = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
